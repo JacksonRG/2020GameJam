984a6ec4491759f6eb9719ce879203133d9a5eb5
Jackson <jackson@ganymede.home>
2020-08-01 09:35:29 -0500

    inventory

---
+++ Assets/Item/Item.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public int maxNumber;
+	public string type;
+	private GameObject knight;
+
+	void Start(){
+        knight = GameObject.FindGameObjectWithTag("Knight").GetComponent<Inventory>();
+	}
+
+	void cast(){
+		switch(type){
+			case "Heal.25":
+				knight.health += knight.maxHealth/4;
+				if (knight.maxHealth < knight.health) {
+					knight.health = knight.maxHealth;
+				}
+				break;
+			case "Heal.5":
+				knight.health += knight.maxHealth/2;
+				if (knight.maxHealth < knight.health) {
+					knight.health = knight.maxHealth;
+				}
+				break;
+			case "Heal.5":
+				knight.health += knight.maxHealth/2;
+				if (knight.maxHealth < knight.health) {
+					knight.health = knight.maxHealth;
+				}
+				break;
+		}
+		
+
+	}
+
+}
