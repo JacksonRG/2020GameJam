63c61fc80cdcce88f328fd5645fcb7b5ab0716c5
Jackson <jackson@ganymede.home>
2020-08-01 09:35:29 -0500

    inventory

---
+++ Assets/Item/Item.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public int maxNumber;
+	public string type;
+	private GameObject knight;
+
+	void Start(){
+        knight = GameObject.FindGameObjectWithTag("Knight").GetComponent<Inventory>();
+	}
+
+	void cast(){
+		switch(type){
+			case "Heal.25":
+				knight.health += knight.maxHealth*.25f;
+				if (knight.maxHealth < knight.health) {
+					knight.health = knight.maxHealth;
+				}
+				break;
+			case "Heal.5":
+				knight.health += knight.maxHealth*.5f;
+				if (knight.maxHealth < knight.health) {
+					knight.health = knight.maxHealth;
+				}
+				break;
+			case "Heal.75":
+				knight.health += knight.maxHealth/0.75f;
+				if (knight.maxHealth < knight.health) {
+					knight.health = knight.maxHealth;
+				}
+				break;
+		}
+		
+
+	}
+
+}
---
+++ Assets/player/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+	//public GameObject itemBar;
+	public GameObject[] slots;
+	private int[] quantity;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		for (int i = 0; i < numSlots; i++){
+			quantity[i] = 0;
+		}
+    }
+
+	void addItem(int slotNum, GameObject item){
+		if (quantity[i] >= item.maxNumber){
+			return;
+		}
+		slots[i] = item;
+		quantity[i] += 1;
+	}
+
+	void useItem(int i){
+		if (quantity <= 0){
+			return;
+		}
+		slots[i].cast();
+		if (quantity[i] > 1){
+			quantity[i] -=1;
+		}
+		else{
+			quantity = 0;
+			Destroy(slots[i]);
+		}
+	}	
+
+}
